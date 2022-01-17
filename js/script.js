f2a827ca11deb298eb57c797329904d5207a34e0
Jelloritimo <83890853+Jelloritimo@users.noreply.github.com>
2022-01-17 16:48:37 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,5 +9,5 @@ const second = d.getSeconds();
 const just_year = `Ⓒ ${year} | Bryce H. Williams | Japan`
 const fulldate = `Last Updated: ${month}/${date}/${year} ${hour}:${minute}:${second}`;
 
-document.getElementById("footer-p-1").textContent = just_year;
+document.querySelector("#footer-p-1").textContent = just_year;
 document.getElementById("footer-p-2").textContent = fulldate;
