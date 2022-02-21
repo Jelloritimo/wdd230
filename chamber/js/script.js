a0334aa0d55b0b2b9649cf24a54de9e311c9047a
Jelloritimo <83890853+Jelloritimo@users.noreply.github.com>
2022-02-21 15:54:20 +0900

    Update script.js

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -11,7 +11,10 @@ const last_update = `Ⓒ ${year} Tokyo-Bay Chamber | Bryce H. Williams | WDD 230
 
 document.getElementById("footer-p-1").textContent = last_update;
 
+// Header date
+
 let headerDateBox = document.getElementById("display-date");
+let meetGreet = document.getElementById("meetgreet");
 
 headerDate();
 
@@ -25,18 +28,15 @@ function headerDate(){
     console.log(requestedFormat.format(Date.now()));
     if (day == 1 || day == 2){
         headerDateBox.append(requestedFormat.format(Date.now()));
+        meetGreet.append(`🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.`);
     }
     else{
         headerDateBox.classList.add("hide");
     }
 }
 
-function toggleMenu(){
-    document.getElementById("navigation").classList.toggle("open");
-    document.getElementById("hamburgerBtn").classList.toggle("open");
-}
-
 // Lazy Load
+
 const images = document.querySelectorAll('[data-src]');
 
 function preloadImage(img) {
@@ -66,6 +66,42 @@ const imgObserver = new IntersectionObserver((entries, imgObserver) => {
 images.forEach(image => {
     imgObserver.observe(image);
 });
+
+// Day count & Visit count
+
+const daycount = document.getElementById("daycount");
+// let numVisits = Number(window.localStorage.getItem("visits-ls"));
+
+let thisVisit = d;
+let lastVisit = window.localStorage.getItem("visitUpdate");
+
+if (lastVisit !== null) {
+    var timeDifference = thisVisit.getTime() - lastVisit;
+    var dayDifference = Math.floor(timeDifference / (1000 * 3600 * 24));
+    if (dayDifference < 1) {
+        daycount.textContent = `Welcome back! Your last visit was today.`;
+    }
+    else if (dayDifference == 1) {
+        daycount.textContent = `Welcome back! Your last visit was ${dayDifference} day ago.`;
+    }
+    else {
+        daycount.textContent = `Welcome back! Your last visit was ${dayDifference} days ago.`;
+    }
+}
+else {
+    daycount.textContent = `Welcome to your first visit!`;
+}
+
+// numVisits++;
+// localStorage.setItem("visits-ls", numVisits);
+localStorage.setItem("visitUpdate", d.getTime());
+
+// Hamburger Button
+
+function toggleMenu(){
+    document.getElementById("navigation").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
 
 const x = document.getElementById("hamburgerBtn");
 x.onclick = toggleMenu; //When user clicks x(hamburgerBtn)...
